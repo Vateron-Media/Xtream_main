bc8834c54967a8fbe6911654ebfd01f28f6a7189
Divarion-D <danil.dmitrievn@gmail.com>
2024-08-23 19:26:15 +0300

    not permisions tmp

--- permissions.sh
+++ permissions.sh
@@ -1,5 +1,5 @@
-chown xtreamcodes:xtreamcodes -R /home/xtreamcodes > /dev/null
 chmod -R 0777 /home/xtreamcodes/bin
+chmod -R 0777 /home/xtreamcodes/tmp
 chmod 0755 /home/xtreamcodes/bin/php/*.pid
 chmod 0755 /home/xtreamcodes/status
 chmod a+x /home/xtreamcodes/status
@@ -9,3 +9,4 @@ sudo find /home/xtreamcodes/admin/ -type d -exec chmod 755 {} \;
 sudo find /home/xtreamcodes/wwwdir/ -type f -exec chmod 644 {} \;
 sudo find /home/xtreamcodes/wwwdir/ -type d -exec chmod 755 {} \;
 sudo chmod +x /home/xtreamcodes/bin/nginx_rtmp/sbin/nginx_rtmp
+chown xtreamcodes:xtreamcodes -R /home/xtreamcodes > /dev/null
