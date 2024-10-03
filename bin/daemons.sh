280c7b23c013fca320e8a760793ef1cca9db3243
Divarion-D <danil.dmitrievn@gmail.com>
2024-10-03 19:13:34 +0300

    moved php pid files

--- bin/daemons.sh
+++ bin/daemons.sh
@@ -1,4 +1,4 @@
 #! /bin/bash
-/sbin/start-stop-daemon --start --quiet --pidfile /home/xtreamcodes/bin/php/1.pid --exec /home/xtreamcodes/bin/php/sbin/php-fpm -- --daemonize --fpm-config /home/xtreamcodes/bin/php/etc/1.conf
-/sbin/start-stop-daemon --start --quiet --pidfile /home/xtreamcodes/bin/php/2.pid --exec /home/xtreamcodes/bin/php/sbin/php-fpm -- --daemonize --fpm-config /home/xtreamcodes/bin/php/etc/2.conf
-/sbin/start-stop-daemon --start --quiet --pidfile /home/xtreamcodes/bin/php/3.pid --exec /home/xtreamcodes/bin/php/sbin/php-fpm -- --daemonize --fpm-config /home/xtreamcodes/bin/php/etc/3.conf
+/sbin/start-stop-daemon --start --quiet --pidfile /home/xtreamcodes/bin/php/sockets/1.pid --exec /home/xtreamcodes/bin/php/sbin/php-fpm -- --daemonize --fpm-config /home/xtreamcodes/bin/php/etc/1.conf
+/sbin/start-stop-daemon --start --quiet --pidfile /home/xtreamcodes/bin/php/sockets/2.pid --exec /home/xtreamcodes/bin/php/sbin/php-fpm -- --daemonize --fpm-config /home/xtreamcodes/bin/php/etc/2.conf
+/sbin/start-stop-daemon --start --quiet --pidfile /home/xtreamcodes/bin/php/sockets/3.pid --exec /home/xtreamcodes/bin/php/sbin/php-fpm -- --daemonize --fpm-config /home/xtreamcodes/bin/php/etc/3.conf
