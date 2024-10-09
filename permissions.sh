57a106057315c09995a79c281c075bb42d225126
Divarion-D <danil.dmitrievn@gmail.com>
2024-10-09 21:23:03 +0300

    new permisions

--- permissions.sh
+++ permissions.sh
@@ -1,11 +1,24 @@
-chmod -R 0777 /home/xtreamcodes/
-chmod 0755 /home/xtreamcodes/bin/php/*.pid
-chmod 0755 /home/xtreamcodes/status
+sudo find /home/xtreamcodes/ -type d -exec chmod 755 {} \;
+sudo find /home/xtreamcodes/ -type f -exec chmod 550 {} \;
+
+chmod 0750 /home/xtreamcodes/bin
+chmod 0750 /home/xtreamcodes/config
+chmod 0750 /home/xtreamcodes/content
+chmod 0750 /home/xtreamcodes/signals
+# chmod 1777 /home/xtreamcodes/tmp
+
+# chmod 0550 /home/xtreamcodes/bin/nginx
+# chmod 0550 /home/xtreamcodes/bin/nginx_rtmp
+# chmod 0550 /home/xtreamcodes/bin/php
+chmod 0771 /home/xtreamcodes/bin/daemons.sh
+chmod 0551 /home/xtreamcodes/bin/ffmpeg
+chmod 0551 /home/xtreamcodes/bin/ffprobe
+
+chmod 0660 /home/xtreamcodes/bin/php/sockets/*
+
+chmod 0644 /home/xtreamcodes/database.sql
+# chmod 0755 /home/xtreamcodes/bin/php/*.pid
 chmod a+x /home/xtreamcodes/status
-chmod 0755 /home/xtreamcodes/service
-sudo find /home/xtreamcodes/admin/ -type f -exec chmod 644 {} \;
-sudo find /home/xtreamcodes/admin/ -type d -exec chmod 755 {} \;
-sudo find /home/xtreamcodes/wwwdir/ -type f -exec chmod 644 {} \;
-sudo find /home/xtreamcodes/wwwdir/ -type d -exec chmod 755 {} \;
+
 sudo chmod +x /home/xtreamcodes/bin/nginx_rtmp/sbin/nginx_rtmp
 chown xtreamcodes:xtreamcodes -R /home/xtreamcodes > /dev/null
