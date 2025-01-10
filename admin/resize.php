409cd3a5426209cf01acf50266dd6c18d24cc131
Divarion-D <danil.dmitrievn@gmail.com>
2025-01-10 19:45:45 +0300

    Improved script for scaling icons

--- admin/resize.php
+++ admin/resize.php
@@ -1,9 +1,46 @@
 <?php
-ini_set('display_errors', 1);
-ini_set('display_startup_errors', 1);
-error_reporting(E_ALL);
+session_start();
+session_write_close();
 
-$rPath = "./icons/";
+if (isset($_SESSION['hash'])) {
+    set_time_limit(2);
+    ini_set('default_socket_timeout', 2);
+
+    $rURL = $_GET['url'];
+    $rMax = intval($_GET['max']);
+
+    $rPath = "./icons/";
+
+    header('Content-Type: image/png');
+    if ($rURL && $rMax) {
+        $rExtension = explode(".", strtolower(pathinfo($rURL)["extension"]))[0];
+        if ($rExtension == "png") {
+            $rImagePath = $rPath . md5($rURL) . "_" . $rMax . ".png";
+            if (!file_exists($rImagePath)) {
+                list($rWidth, $rHeight) = getimagesize($rURL);
+                $rImageSize = getImageSizeKeepAspectRatio($rURL, $rMax, $rMax);
+                if (($rImageSize["width"]) && ($rImageSize["height"])) {
+                    $rImageP = imagecreatetruecolor($rImageSize["width"], $rImageSize["height"]);
+                    $rImage = imagecreatefrompng($rURL);
+                    imagealphablending($rImageP, false);
+                    imagesavealpha($rImageP, true);
+                    imagecopyresampled($rImageP, $rImage, 0, 0, 0, 0, $rImageSize["width"], $rImageSize["height"], $rWidth, $rHeight);
+                    imagepng($rImageP, $rImagePath);
+                }
+            }
+            if (file_exists($rImagePath)) {
+                echo file_get_contents($rImagePath);
+                exit;
+            }
+        }
+    }
+    $rImage = imagecreatetruecolor(1, 1);
+    imagesavealpha($rImage, true);
+    imagefill($rImage, 0, 0, imagecolorallocatealpha($rImage, 0, 0, 0, 127));
+    imagepng($rImage);
+} else {
+    exit();
+}
 
 function getImageSizeKeepAspectRatio($imageUrl, $maxWidth, $maxHeight) {
     $imageDimensions = getimagesize($imageUrl);
@@ -22,34 +59,3 @@ function getImageSizeKeepAspectRatio($imageUrl, $maxWidth, $maxHeight) {
     }
     return $imageSize;
 }
-
-$rURL = ipTV_lib::$request["url"];
-$rMax = ipTV_lib::$request["max"];
-
-header('Content-Type: image/png');
-if ($rURL && $rMax) {
-    $rExtension = explode(".", strtolower(pathinfo($rURL)["extension"]))[0];
-    if ($rExtension == "png") {
-        $rImagePath = $rPath . md5($rURL) . "_" . $rMax . ".png";
-        if (!file_exists($rImagePath)) {
-            list($rWidth, $rHeight) = getimagesize($rURL);
-            $rImageSize = getImageSizeKeepAspectRatio($rURL, $rMax, $rMax);
-            if (($rImageSize["width"]) && ($rImageSize["height"])) {
-                $rImageP = imagecreatetruecolor($rImageSize["width"], $rImageSize["height"]);
-                $rImage = imagecreatefrompng($rURL);
-                imagealphablending($rImageP, false);
-                imagesavealpha($rImageP, true);
-                imagecopyresampled($rImageP, $rImage, 0, 0, 0, 0, $rImageSize["width"], $rImageSize["height"], $rWidth, $rHeight);
-                imagepng($rImageP, $rImagePath);
-            }
-        }
-        if (file_exists($rImagePath)) {
-            echo file_get_contents($rImagePath);
-            exit;
-        }
-    }
-}
-$rImage = imagecreatetruecolor(1, 1);
-imagesavealpha($rImage, true);
-imagefill($rImage, 0, 0, imagecolorallocatealpha($rImage, 0, 0, 0, 127));
-imagepng($rImage);
